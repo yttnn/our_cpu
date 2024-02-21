e2fc04040f798dc3ca714fbc87359c3148caf129
yttnn <67893139+yttnn@users.noreply.github.com>
2024-02-21 18:55:39 +0900

    wip

--- core/core.sv
+++ core/core.sv
@@ -8,11 +8,6 @@ module core (
   logic [31:0] data;
   logic [31:0] inst;
   
-  always_comb begin
-    case(inst)
-      
-    endcase
-  end
   // IF
 
   // ID
@@ -23,8 +18,23 @@ module core (
   wire [4:0] rd       = inst[11:7];
   wire [6:0] opcode  = inst[6:0];
 
+ 
+  wire [11:0] imm_i = inst[31:20];
+  wire [31:0] imm_i_sext = {{20{imm_i[11]}}, imm_i};
+  wire [11:0] imm_s = {inst[31:25], inst[11:7]};
+  wire [12:0] imm_b = {inst[31], inst[7], inst[30:25], inst[11:8], 1'b0}; // imm_b[0] = 0 ????
+  wire [31:0] imm_u = {inst[31:12], 12'b0};
+  wire [20:0] imm_j = {inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
+
   wire [31:0] rs1_data = (rs1_addr != '0) ? registers[rs1_addr] : '0;
   wire [31:0] rs2_data = (rs2_addr != '0) ? registers[rs2_addr] : '0;
+
+  // EX
+  always @(posedge clk) begin
+    if ((opcode == 7'b0110011) && (funct3 == 3'b000) && (funct7 == 7'b0000000)) begin
+      registers[rd] = rs1_data + rs2_data;
+    end
+  end
 
   always @(posedge clk ) begin
     pc = pc + 4;
