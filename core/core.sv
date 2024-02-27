52c365c252174267de4e3225b3f6d5a887eb6f25
yttnn <67893139+yttnn@users.noreply.github.com>
2024-02-27 14:16:25 +0900

    rm comment

--- core/core.sv
+++ core/core.sv
@@ -43,11 +43,6 @@ module core (
     
   end
   
-  // IF
-
-  // instruction_memory _instruction_memory(.addr(pc), .inst(inst));
-
-  // ID
   wire [6:0] funct7   = inst[31:25];
   wire [4:0] rs2_addr = inst[24:20];
   wire [4:0] rs1_addr = inst[19:15];
@@ -76,47 +71,6 @@ module core (
   wire [31:0] imm_b_sign_ext = {{19{imm_b[12]}}, imm_b};
   wire [31:0] imm_u_sign_ext = {imm_u};
   wire [31:0] imm_j_sign_ext = {{11{imm_j[20]}}, imm_j};
-  // instinfo_t instinfo;
-  // wire [4:0] rs1_addr;
-  // wire [4:0] rs2_addr;
-  // decode _decode(
-  //   .clk(clk), .reset_n(reset_n), .inst_raw(inst),
-  //   .rs1_addr(rs1_addr), .rs2_addr(rs2_addr), .instinfo(instinfo)
-  // );
-
-  // wire [31:0] rs1_data = (rs1_addr != '0) ? registers[rs1_addr] : '0;
-  // wire [31:0] rs2_data = (rs2_addr != '0) ? registers[rs2_addr] : '0;
-
-  // instructions decode_inst;
-
-  // always @(posedge clk) begin
-  //   decode_inst.add <= ((opcode == 7'b0110011) && (funct3 == 3'b000) && (funct7 == 7'b0000000));
-  // end
-
-  // EX
-  // wire [31:0] alu_out;
-  // alu _alu(
-  //   .clk(clk), .reset_n(reset_n), .instinfo(instinfo),
-  //   .op1_data(rs1_data), .op2_data(rs2_data),
-  //   .alu_out(alu_out)
-  // );
-  // always @(posedge clk) begin
-  //   $display("%b", inst);
-    // decode_inst.add = ((opcode == 7'b0110011) && (funct3 == 3'b000) && (funct7 == 7'b0000000));
-    // decode_inst.sub = ((opcode == 7'b0110011) && (funct3 == 3'b000) && (funct7 == 7'b0100000));
-    // decode_inst.addi = ((opcode == 7'b0010011) && (funct3 == 3'b000));
-  //   if (decode_inst.add) begin
-  //     registers[rd] = rs1_data + rs2_data;
-  //     $display("add");
-  //   end
-  //   else if (decode_inst.sub) begin
-  //     registers[rd] = rs1_data - rs2_data;
-  //   end
-  //   else if (decode_inst.addi) begin
-  //     registers[rd] = rs1_data + imm_i_sext;
-  //   end
-  //   pc = pc + 1;
-  // end
 
   always @(posedge clk ) begin
     if (!reset_n) begin
