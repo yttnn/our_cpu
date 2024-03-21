8569ef26833f4b5216c8dc653f50d36722187dcd
yttnn <67893139+yttnn@users.noreply.github.com>
2024-03-21 10:26:36 +0900

    add test

--- core/memory.sv
+++ core/memory.sv
@@ -15,7 +15,45 @@ module memory (
   logic [7:0] ROM [0: 16384];
   initial begin
     $readmemh("../riscv-tests/hex/rv32ui-p-add.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-addi.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-and.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-andi.hex", ROM);
     // $readmemh("../riscv-tests/hex/rv32ui-p-auipc.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-beq.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-bge.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-bgeu.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-blt.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-bltu.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-bne.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-fence_i.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-jal.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-jalr.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-lb.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-lbu.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-lh.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-lhu.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-lui.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-lw.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-ma_data.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-or.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-ori.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-sb.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-sh.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-simple.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-sll.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-slli.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-slt.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-slti.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-sltiu.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-sltu.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-sra.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-srai.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-srl.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-srli.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-sub.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-sw.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-xor.hex", ROM);
+    // $readmemh("../riscv-tests/hex/rv32ui-p-xori.hex", ROM);
   end
 
   `include "riscv_assembly.v"
