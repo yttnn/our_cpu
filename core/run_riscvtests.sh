12e8bceb9d573e7dff071624c986be3b7099c6c6
yttnn <67893139+yttnn@users.noreply.github.com>
2024-03-29 11:33:19 +0900

    add riscvtests support

---
+++ core/run_riscvtests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+tests=(
+  "rv32ui-p-add" "rv32ui-p-addi" "rv32ui-p-and" "rv32ui-p-andi" "rv32ui-p-auipc"
+  "rv32ui-p-beq" "rv32ui-p-bge" "rv32ui-p-bgeu" "rv32ui-p-blt" "rv32ui-p-bltu" "rv32ui-p-bne"
+  "rv32ui-p-fence_i"
+  "rv32ui-p-jal" "rv32ui-p-jalr"
+)
+
+iverilog -g2012 -DBENCH testbench_iverilog.sv top.sv memory_for_riscvtests.sv core.sv def.sv
+
+for test_case in "${tests[@]}"
+do
+  echo "case : $test_case"
+  testfile="../riscv-tests/hex/$test_case.hex"
+  if [ ! -e $testfile ]; then
+    echo "error: not found $test_case.hex from run_riscvtests.sh"
+    continue
+  fi
+
+  vvp a.out +INPUT_FILE=$testfile
+
+  echo "-------------"
+done
